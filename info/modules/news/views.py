7a24681366e1a48d7ecfe783b373211f4b72e07e
lecis1 <lecis19521@163.com>
2020-11-21 15:28:29 +0800

    基类模板抽取

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -2,9 +2,33 @@
 # @Author:lecis
 # @Time:2020/11/21 11:50
 # @ Software:PyCharm
+from flask import current_app, jsonify, render_template, abort
+
 from . import news_blue
 
 
-@news_blue.route('/news_detail')
-def news_detail():
-    return "展示新闻详情"
+# 请求路径：/news/<int:news_id>
+# 请求方式：GET
+# 请求参数：news_id
+# 返回值：detail.html页面，用户data字典数据
+from ...models import News
+from ...utils.response_code import RET
+
+
+@news_blue.route('/<int:news_id>')
+def news_detail(news_id):
+    # 1.根据新闻编号查询新闻对象
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger(e)
+        return jsonify(errno=RET.DBERR, errmsg="获取新闻失败")
+
+    # 如果新闻对像不存在直接抛出异常、
+    if not news:
+        abort(404)
+    # 2.携带数据渲染页面
+    data = {
+        "news_info": news.to_dict() ,
+    }
+    return render_template('news/detail.html', data=data)
