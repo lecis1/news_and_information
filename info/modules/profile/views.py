61e3d7279268ff93d0851d3a87e0176b3f0002ec
lecis1 <lecis19521@163.com>
2020-11-23 17:56:04 +0800

    新闻发布的实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -3,17 +3,19 @@
 # @Time:2020/11/23 9:56
 # @ Software:PyCharm
 from . import profile_blue
-from flask import render_template, g, redirect, request, jsonify
+from flask import render_template, g, redirect, request, jsonify, current_app
+from info.utils.image_storage import image_storage
+from ...models import News, Category
+from ...utils.comments import user_login_data
+from ...utils.response_code import RET
+from info import constants, db
+
 
 # 获取用户首页信息
 # 请求路径：/user/info
 # 请求方式：GET
 # 请求参数：无
 # 返回值：user.html页面，用户字典data
-from ...utils.comments import user_login_data
-from ...utils.response_code import RET
-
-
 @profile_blue.route('/info')
 @user_login_data
 def user_index():
@@ -68,16 +70,36 @@ def base_info():
 @user_login_data
 def pic_info():
     # 1.判断请求方式，如果是get请求
+    # 2携带用户的数据进行渲染
     if request.method == 'GET':
         return render_template('news/user_pic_info.html', user_info=g.user.to_dict())
-    # 2携带用户的数据进行渲染
+
     # 3.若果是post请求
     # 4.参数获取
+    avatar = request.files.get('avatar')
+
     # 5.校验参数，为空校验
+    if not avatar:
+        return jsonify(errno=RET.DATAERR, errmsg="图片不能为空")
     # 6.上传头像
+    try:
+        # 读取图片为二进制上传
+        image_name = image_storage(avatar.read())
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="七牛云异常")
     # 7.判断图片是否上传成功
+    if not image_name:
+        return jsonify(errno=RET.NODATA, errmsg="图片上传失败")
     # 8.将图片设置到用户对象
+    g.user.avatar_url = image_name
     # 9.返回响应
+
+
+    data = {
+        "avatar_url": constants.QINIU_DOMIN_PREFIX + image_name
+    }
+    return jsonify(errno=RET.OK, errmsg="上传成功", data=data)
     
     
 # 获取/设置用户密码
@@ -106,3 +128,113 @@ def pass_info():
 
     # 7.返回响应
     return jsonify(errno=RET.OK, errmsg="密码修改成功")
+
+
+# 获取新闻收藏列表
+# 请求路径：/user/colletion
+# 请求方式：GET
+# 请求参数：page
+# 返回值：user_colletion.html页面
+@profile_blue.route('/collection')
+@user_login_data
+def collection():
+    # 1.获取参数P
+    page = request.args.get('p', '1')
+
+    # 2.参数类型转换
+    try:
+        page = int(page)
+    except Exception as e:
+        page = 1
+
+    # 3.分页查询收藏的新闻
+    try:
+        paginate = g.user.collection_news.order_by(News.create_time.desc()).paginate(page, 2, False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="获取新闻失败")
+
+    # 4.获取分页对象属性，总页数，当前页，当前页对象列表
+    totalPage = paginate.pages
+    current_page = paginate.page
+    items = paginate.items
+    # 5.将对象列表转成字典列表
+    news_list = []
+    for news in items:
+        news_list.append(news.to_dict())
+
+    # 6.拼接数据进行渲染
+    data = {
+        "totalPage": totalPage,
+        "current_page": current_page,
+        "news_list": news_list,
+    }
+
+    return render_template('news/user_collection.html', data=data)
+
+
+# 获取/设置新闻发布
+# 请求路径：/user/news_release
+# 请求方式：GET, POST
+# 请求参数：GET无，POST:title,category_id, digest, index_image, content
+# 返回值：GET请求，user_news, release.html, data分类列表字段数据，POST:errno, errmsg
+@profile_blue.route('/news_release', methods=['GET', 'POST'])
+@user_login_data
+def news_release():
+    # 1.判断请求方式，如果是GET
+    if request.method == "GET":
+        # 2.1查询所有的分类数据
+        try:
+            categories = Category.query.all()
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(errno=RET.DBERR, errmsg="获取分类失败")
+
+        category_list = []
+        for category in categories:
+            category_list.append(category.to_dict())
+        # 2.携带数据渲染页面
+        return render_template('news/user_news_release.html', categories=category_list)
+
+    # 3.如果是post，获取参数
+    title = request.form.get("title")
+    category_id = request.form.get("category_id")
+    digest = request.form.get("digest")
+    index_image = request.files.get("index_image")
+    content = request.form.get("content")
+    # 4.校验参数，为空校验
+    if not all([title, category_id, digest, index_image, content]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+
+    # 5.上传图片，判断是否上传成功
+    try:
+        # 读取图片位二进制数据上传
+        image_name = image_storage(index_image.read())
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg='七牛云异常')
+
+    if not image_name:
+        return jsonify(errno=RET.NODATA, errmsg="图片上传失败")
+
+    # 6.创建新闻对象，设置属性
+    news = News()
+    news.title = title
+    news.source = g.user.nick_name
+    news.digest = digest
+    news.content = content
+    news.index_image_url = constants.QINIU_DOMIN_PREFIX + image_name
+    news.category_id = category_id
+    news.user_id = g.user.id
+    news.status = 1  # 表示审核中
+
+    # 7.保存到数据库
+    try:
+        db.session.add(news)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="新闻发布失败")
+
+    # 8.返回响应
+    return jsonify(errno=RET.OK, errmsg="发布成功")
---
+++ info/utils/image_storage.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Author:lecis
+# @Time:2020/11/23 15:16
+# @ Software:PyCharm
+from qiniu import Auth, put_file, etag, put_data
+import qiniu.config
+
+# 需要填写你的 Access Key 和 Secret Key
+access_key = 'pPzV3IuBFze_kqQb-wEe7BRkElbNu5ivCmYzL08M'
+secret_key = 'j9muVRiSR5MAg18dxY2o7WXtacglsxWuY92Sy_fe'
+
+
+def image_storage(image_data):
+    # 构建鉴权对象
+    q = Auth(access_key, secret_key)
+
+    # 要上传的空间
+    bucket_name = 'lecis'
+
+    # 上传后保存的文件名,如果不指定，那么名字由七牛云维护
+    key = None
+
+    # 生成上传 Token，可以指定过期时间等
+    token = q.upload_token(bucket_name, key, 3600)
+
+    # 要上传文件的本地路径
+    # localfile = './沪.jpg'
+
+    # ret, info = put_file(token, key, localfile)
+    ret, info = put_data(token, key, image_data)
+
+    # 如果上传成功返回图片名字，否则返回None
+    if info.status_code == 200:
+
+        # print(info)
+        # print(ret)
+        return ret.get('key')
+    else:
+        return None
+    # assert ret['key'] == key
+    # assert ret['hash'] == etag(localfile)
+
+
+# 测试图片上传
+if __name__ == "__main__":
+    # 使用with测试，可以自动关闭
+    with open('./沪.jpg', "rb") as f:
+        image_storage(f.read())
